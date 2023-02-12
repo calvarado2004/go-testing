a1c10b2b57885c5663019f2b02b72e33422c37b4
calvarado2004 <carlos-alvarado@outlook.com>
2023-02-12 14:49:46 -0500

    making the program runnable

--- main.go
+++ main.go
@@ -1,17 +1,88 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 // main function
 func main() {
 
-	n := 123234
+	//print a welcome message
+	intro()
 
-	isPrime(n)
+	//crete a channel to indicate when the user wants to quit
+	doneChan := make(chan bool)
 
+	// start a goroutine to read user input and run program
+	go readUserInput(doneChan)
+
+	// block until the doneChan gets a value
+	<-doneChan
+
+	// close the channel
+	close(doneChan)
+
+	// print a goodbye message
+	fmt.Println("Goodbye")
+
+}
+
+// readUserInput reads user input and runs the program
+func readUserInput(doneChan chan bool) {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		res, done := checkNumbers(scanner)
+
+		if done {
+			doneChan <- true
+			return
+		}
+
+		fmt.Println(res)
+		prompt()
+	}
+}
+
+// checkNumbers checks if the user input is a valid number
+func checkNumbers(scanner *bufio.Scanner) (string, bool) {
+
+	// read user input
+	scanner.Scan()
+
+	// check if the user wants to quit
+	if strings.EqualFold(scanner.Text(), "q") {
+		return "", true
+	}
+
+	// convert the user input to an integer
+	n, err := strconv.Atoi(scanner.Text())
+
+	if err != nil {
+		return "Please enter a valid integer number", false
+	}
+
+	// check if the number is prime
 	_, msg := isPrime(n)
-	fmt.Println(msg)
 
+	return msg, false
+}
+
+// intro prints a welcome message
+func intro() {
+	fmt.Println("Welcome to the prime number checker")
+	fmt.Println("Enter a number to check if it is prime")
+	fmt.Println("Enter 'q' to quit")
+	prompt()
+}
+
+// prompt prints a prompt to the user
+func prompt() {
+	fmt.Print("Enter a number: ")
 }
 
 // isPrime returns a boolean value and a string
