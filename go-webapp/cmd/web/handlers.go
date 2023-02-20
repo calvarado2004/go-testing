1cd2266e3c5c920f1c3cc25424d43b2967eb29a0
calvarado2004 <carlos-alvarado@outlook.com>
2023-02-20 18:47:29 -0500

    adding home page

---
+++ go-webapp/cmd/web/handlers.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func (app *application) Home(w http.ResponseWriter, r *http.Request) {
+
+	err := app.render(w, r, "home.page.gohtml", &TemplateData{})
+	if err != nil {
+		log.Printf("error rendering template: %v", err)
+	}
+
+}
+
+type TemplateData struct {
+	IP   string
+	Data map[string]any
+}
+
+func (app *application) render(w http.ResponseWriter, r *http.Request, t string, data *TemplateData) error {
+
+	// parse the template from disk
+	parsedTemplate, err := template.ParseFiles("./templates/" + t)
+	if err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return err
+	}
+
+	// write the template to the http.ResponseWriter
+	err = parsedTemplate.Execute(w, data)
+	if err != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return err
+	}
+
+	return nil
+}
