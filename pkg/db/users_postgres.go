63983b1c7c631fa58be70308533c2b2acc13de78
calvarado2004 <carlos-alvarado@outlook.com>
2023-03-03 00:42:35 -0500

    cleaning modules

--- pkg/db/users_postgres.go
+++ pkg/db/users_postgres.go
@@ -3,7 +3,7 @@ package db
 import (
 	"context"
 	"database/sql"
-	"github.com/calvarado2004/go-testing/go-webapp/webapp/pkg/data"
+	"github.com/calvarado2004/go-testing/pkg/data"
 	"golang.org/x/crypto/bcrypt"
 	"log"
 	"time"
