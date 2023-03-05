55bf6f773263117ba2f74d14180efd3478a49ca7
calvarado2004 <carlos-alvarado@outlook.com>
2023-03-05 16:00:55 -0500

    fixing routes

--- go-webapp/cmd/web/routes.go
+++ go-webapp/cmd/web/routes.go
@@ -15,15 +15,15 @@ func (app *application) routes() http.Handler {
 	mux.Use(app.addIPToContext)
 	mux.Use(app.Session.LoadAndSave)
 
-	// register routes
-	mux.Get("/", app.Home)
-	mux.Post("/login", app.Login)
+	mux.Route("/", func(r chi.Router) {
+		mux.Get("/", app.Home)
+		mux.Post("/login", app.Login)
 
-	mux.Route("/user", func(r chi.Router) {
-		mux.Use(app.auth)
-		mux.Get("/profile", app.Profile)
+		mux.With(app.auth)
+		mux.Get("/user/profile", app.Profile)
+
 	})
-	
+
 	// static files
 	fileServer := http.FileServer(http.Dir("./static/"))
 
