e1cba242dc393741641900a30c4e2e42dfbb934e
calvarado2004 <carlos-alvarado@outlook.com>
2023-03-09 22:46:20 -0500

    creating docker test postgres db

--- go-webapp/pkg/repository/dbrepo/users_postgres_test.go
+++ go-webapp/pkg/repository/dbrepo/users_postgres_test.go
@@ -2,10 +2,13 @@ package dbrepo
 
 import (
 	"database/sql"
+	"fmt"
 	_ "github.com/jackc/pgconn"
 	_ "github.com/jackc/pgx/v4"
 	_ "github.com/jackc/pgx/v4/stdlib"
 	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
+	"log"
 	"os"
 	"testing"
 )
@@ -27,7 +30,62 @@ var testDB *sql.DB
 
 // TestMain is the entry point for all tests
 func TestMain(m *testing.M) {
+
+	// connect to docker; fail if docker is not running
+	p, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %v", err)
+	}
+
+	pool = p
+
+	// set up docker options, specifying image, port, and env vars
+	dockerOpts := dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "14.5",
+		Env: []string{
+			"POSTGRES_USER=" + user,
+			"POSTGRES_PASSWORD=" + password,
+			"POSTGRES_DB=" + dbname,
+		},
+		ExposedPorts: []string{"5432"},
+		PortBindings: map[docker.Port][]docker.PortBinding{
+			"5432": {
+				{
+					HostIP:   "0.0.0.0",
+					HostPort: port,
+				},
+			},
+		},
+	}
+
+	// get docker image
+	resource, err = pool.RunWithOptions(&dockerOpts)
+	if err != nil {
+		log.Fatalf("Could not start resource: %v", err)
+	}
+
+	// start the docker container and wait for it to be ready
+	if err = pool.Retry(func() error {
+		var err error
+		testDB, err = sql.Open("pgx", fmt.Sprintf(dsn, host, port, user, password, dbname))
+		if err != nil {
+			log.Println("Could not connect to postgres yet")
+			return err
+		}
+		return testDB.Ping()
+	}); err != nil {
+		_ = pool.Purge(resource)
+		log.Fatalf("Could not connect to postgres at all: %s", err)
+	}
+
+	// populate the test database
+
+	// run the tests
+
 	code := m.Run()
+
+	// clean up after tests
 
 	os.Exit(code)
 }
